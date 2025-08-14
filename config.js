ec3579226feb73f8585672b22475a4e902ffcd5e
vaishnavaaditya848-debug <vaishnavaaditya848@gmail.com>
2025-08-14 21:16:46 +0530

    Update config.js
    Signed-off-by: vaishnavaaditya848-debug <vaishnavaaditya848@gmail.com>

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // If you deploy a backend, paste its URL here.
 // Leave empty to use offline fallback.
-const BACKEND_URL = "https://script.google.com/macros/s/AKfycbyVDrhMB2zN7axHB7Ps6hn0zXeLGJG-atkcDMIEbhVZ_kY5qpJXmdyfsmr1arTe8xsK/exec"; // e.g., 'https://script.google.com/macros/s/AKfycbXYZ/exec'
+const BACKEND_URL = "https://script.google.com/macros/s/AKfycbwTearpbPnyxEdiwaLN5jWYrgT6fv8u3cy9fNGe4b70E803Ilay2HORlirUqp9qEaqn/exec"; // e.g., 'https://script.google.com/macros/s/AKfycbXYZ/exec'
